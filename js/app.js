d243be0d2b0fa31948ebd7cc0c8f7178e87fe175
subhan <sub.metu@gmail.com>
2016-08-27 23:09:36 +0300

    First Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var $overlay=$(' <div id="overlay"></div>');
+var $image=$('<img>');
+var $heading=$('<h2></h2>')
+var $caption=$('<span></span>');
+$overlay.append($image);
+$heading.append($caption);
+$overlay.append($heading);
+$("body").append($overlay);
+
+
+$("#food-list img").click(function(){
+    var source=$(this).attr("src");
+    var caption=$(this).attr("title");
+    $caption.text(caption);
+    $image.attr("src",source);
+    $overlay.fadeIn(300);
+});
+
+$(overlay).click(function(){
+    $overlay.fadeOut(200);
+});
